5e3736f64f94f475921f1f13272c6e8dedf10363
Tsierenana Botramanagna Gracy <gtsierenana@gmail.com>
2024-08-12 21:08:43 +0300

    refactor: Format code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
-function App() {
+import Button from "./components/Button";
+import "./index.css";
 
+function App() {
   return (
     <>
+      <Button icon={<p>g</p>} onClick={() => {}} primary>
+        Creer nouveau doc
+      </Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
