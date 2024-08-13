11d173ec66d39f814acc3e12c73b7baf2db6753d
Tsierenana Botramanagna Gracy <gtsierenana@gmail.com>
2024-08-13 12:56:17 +0300

    ui: Add state indicator to navlink

--- frontend/src/components/Navlink/Navlink.tsx
+++ frontend/src/components/Navlink/Navlink.tsx
@@ -4,21 +4,21 @@ import clsx from "clsx";
 type NavlinkProps = {
   target: string;
   icon: ReactNode;
-  current: boolean;
+  current?: boolean;
   children: ReactNode;
 };
 
 export default function Navlink({
   target,
   icon,
-  current,
+  current = false,
   children,
 }: NavlinkProps) {
   return (
     <a
       href={target}
       className={clsx({
-        "flex gap-2 hover:bg-[rgba(255,255,255,0.14)] py-2 px-4":
+        "flex gap-2 hover:bg-[rgba(255,255,255,0.14)] py-2 px-4 cursor-pointer":
           true,
         "text-white": current,
         "text-[#cdcdcd]": !current,
