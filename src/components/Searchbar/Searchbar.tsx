94c2162c53cfa2962a607cb73a39241495c300ef
Tsierenana Botramanagna Gracy <gtsierenana@gmail.com>
2024-08-29 15:42:28 +0300

    feat: Focus on input field wherever I click on the search bar

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,5 +1,6 @@
 import { Search } from "@mui/icons-material";
 import clsx from "clsx";
+import { useRef } from "react";
 
 export default function Searchbar({
   onTyping,
@@ -8,15 +9,23 @@ export default function Searchbar({
   onTyping: (text: string) => void;
   disabled?: boolean;
 }) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  function handleClick() {
+    inputRef.current?.focus()
+  }
+
   return (
     <div
       className={clsx({
         "bg-[#f2f2f2] h-fit rounded-[20px] p-4 flex gap-2 w-full": true,
         "cursor-not-allowed *:cursor-not-allowed": disabled,
       })}
+      onClick={handleClick}
     >
       <Search htmlColor="#cdcdcd" />
       <input
+        ref={inputRef}
         disabled={disabled}
         onChange={(e) => onTyping(e.target.value)}
         type="text"
