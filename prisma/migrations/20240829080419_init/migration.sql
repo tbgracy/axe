7cc571da2d148eb442363fba1a4e2d893e489643
Tsierenana Botramanagna Gracy <gtsierenana@gmail.com>
2024-08-29 12:28:25 +0300

    database: Change model

---
+++ prisma/migrations/20240829080419_init/migration.sql
@@ -0,0 +1,15 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "creationDate" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "profilPicturePath" TEXT
+);
+INSERT INTO "new_User" ("creationDate", "email", "id", "profilPicturePath", "username") SELECT "creationDate", "email", "id", "profilPicturePath", "username" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
