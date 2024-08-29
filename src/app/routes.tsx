98609577a0aae95d8967f7121fac8e0c6f10024a
Tsierenana Botramanagna Gracy <gtsierenana@gmail.com>
2024-08-29 12:28:12 +0300

    feat: Add splash screen

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -3,18 +3,23 @@ import { createBrowserRouter } from "react-router-dom";
 import DocumentManager from "../screens/DocumentManager/DocumentManager";
 import Editor from "../screens/Editor";
 import Layout from "./Layout";
+import Splashscreen from "../screens/Splashscreen";
 
 const router: ReturnType<typeof createBrowserRouter> = createBrowserRouter([
   {
     path: "/",
+    element: <Splashscreen />,
+  },
+  {
+    path: "/app",
     element: <Layout />,
     children: [
       {
-        path: "/documents",
+        path: "documents",
         element: <DocumentManager />,
       },
       {
-        path: "/documents/:documentId",
+        path: "documents/:documentId",
         element: <Editor />,
       },
     ],
---
+++ src/screens/Splashscreen/Splashscreen.tsx
@@ -0,0 +1,27 @@
+import Logo from "../../components/Logo";
+import { Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+export default function Splashscreen() {
+  const [status, setStatus] = useState<"initializing" | "done">("initializing");
+
+  useEffect(() => {
+    if (status === "initializing") {
+      new Promise((r) => setTimeout(r, 2000)).then(() => setStatus("done"));
+    }
+  }, [status]);
+
+  if (status === "initializing") {
+    return (
+      <div className="w-screen h-screen flex flex-col items-center justify-center">
+        <Logo />
+        <h1 className="text-primary">Axe</h1>
+        <p className="absolute bottom-2">Veuillez patienter ... </p>
+      </div>
+    );
+  }
+
+  if (status === "done") {
+    return <Navigate to="/app" />;
+  }
+}
