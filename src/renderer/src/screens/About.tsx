a2ca3ec2c5a1021e24c2b401832b46e7e34c6965
Tsierenana Botramanagna Gracy <gtsierenana@gmail.com>
2024-11-12 23:12:49 +0300

    minor fixes

--- src/renderer/src/screens/About.tsx
+++ src/renderer/src/screens/About.tsx
@@ -3,13 +3,15 @@ import Logo from "@renderer/components/Logo";
 export default function About() {
   return (
     <div className="flex flex-col items-center justify-center text-center text-sm w-full h-screen gap-3 dark:bg-darkGrey dark:text-white">
-      <div>
+      <div className="flex flex-col items-center">
         <Logo />
-        <h1 className="text-primary">Axe</h1>
+        <h1>
+          À propos de <span className="text-primary">Axe</span>
+        </h1>
       </div>
       <p className="text-base">Éditeur de texte collaboratif "local-first".</p>
       <p>Version : 0.1.0 </p>
-      <p>&copy; Tsierenana Botramanagna Gracy - 2024</p>
+      <p>&copy; 2024 - Tsierenana Botramanagna Gracy</p>
     </div>
   );
 }
