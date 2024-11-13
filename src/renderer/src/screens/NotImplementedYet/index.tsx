ca11177d45dbb56f2e916501ffe575027f41da13
Tsierenana Botramanagna Gracy <gtsierenana@gmail.com>
2024-11-13 06:38:59 +0300

    fix: dark mode

--- src/renderer/src/screens/NotImplementedYet/index.tsx
+++ src/renderer/src/screens/NotImplementedYet/index.tsx
@@ -3,7 +3,7 @@ import illustration from "./undraw_under_construction_-46-pa.svg";
 
 export default function NotImplementedYet() {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen">
+    <div className="flex flex-col items-center justify-center w-screen h-screen dark:text-white dark:bg-darkGrey">
       <img src={illustration} alt="illustration" className="size-[300px] animate-float" />
       Cette partie de l'application est encore en construction.
     </div>
